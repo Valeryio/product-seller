b3c6259da870cb6694b92346e4d37ea29dd3f61b
valeryio <tommagoma23@gmail.com>
2025-06-04 18:10:53 +0200

    Adding the vercel frontend ip address

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -3,7 +3,8 @@ const allowedOrigins = [
 	"http://localhost:5173/",
 	"http://localhost:5173/login/",
 	"http://localhost:5173/register/",
-	"http://41.138.91.152:5173/"
+	"http://41.138.91.152:5173/",
+	"https://product-seller.vercel.app/"
 ];
 
 module.exports = allowedOrigins;
