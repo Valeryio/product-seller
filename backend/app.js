6dfbb846c668d932e5c1d19f49829b9d125dc4bc
valeryio <tommagoma23@gmail.com>
2025-06-04 17:56:24 +0200

    Update the cors policy

--- backend/app.js
+++ backend/app.js
@@ -17,11 +17,8 @@ require("./models/dbConfig");
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
-// app.use(cors({
-//   origin: "http://localhost:5173",
-//   credentials: true
-// }));
+app.use(cors(corsOptions));
+
 
 app.use("/", express.static(path.join(__dirname, "/public")));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -2,7 +2,8 @@
 const allowedOrigins = [
 	"http://localhost:5173/",
 	"http://localhost:5173/login/",
-	"http://localhost:5173/register/"
+	"http://localhost:5173/register/",
+	"http://41.138.91.152:5173/"
 ];
 
 module.exports = allowedOrigins;
