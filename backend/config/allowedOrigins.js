87253be8ebfa6280c30c9223628fe0a04ac1bb0b
valeryio <tommagoma23@gmail.com>
2025-06-05 12:42:33 +0200

    Update the origin file

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -4,7 +4,8 @@ const allowedOrigins = [
 	"http://localhost:5173/login/",
 	"http://localhost:5173/register/",
 	"http://41.138.91.152:5173/",
-	"https://product-seller.vercel.app/"
+	"https://product-seller.vercel.app/",
+	"https://product-seller-k56z8mkjv-valeryios-projects.vercel.app/"
 ];
 
 module.exports = allowedOrigins;
--- backend/config/corsOptions.js
+++ backend/config/corsOptions.js
@@ -1,9 +1,9 @@
 
-const allowedOrigins = required("./allowedOrigins");
+const allowedOrigins = require("./allowedOrigins");
 
 const corsOptions = {
 	origin: (origin, callback) => {
-		if (allowedOrigins.indexOf(origin) !== -1) {
+		if (!origin || allowedOrigins.indexOf(origin) !== -1) {
 			callback(null, true);
 		} else {
 			callback(new Error("Not allowed by CORS"));
