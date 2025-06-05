21144e925d77f53ea278bd8ceb092167a56baa92
valeryio <tommagoma23@gmail.com>
2025-06-05 14:19:28 +0200

    Update the authorized IP Address

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -5,6 +5,8 @@ const allowedOrigins = [
 	"http://localhost:5173/register/",
 	"http://41.138.91.152:5173/",
 	"https://product-seller.vercel.app/",
+	"https://product-seller.vercel.app/login",
+	"https://product-seller.vercel.app/register",
 	"https://product-seller-k56z8mkjv-valeryios-projects.vercel.app/"
 ];
 
