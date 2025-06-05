598f789989675f9b54617085add940b5b15e5e9c
valeryio <tommagoma23@gmail.com>
2025-06-05 14:29:21 +0200

    Remove no origin requesters

--- backend/config/corsOptions.js
+++ backend/config/corsOptions.js
@@ -3,7 +3,7 @@ const allowedOrigins = require("./allowedOrigins");
 
 const corsOptions = {
 	origin: (origin, callback) => {
-		if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+		if (origin || allowedOrigins.includes(origin)) {
 			callback(null, true);
 		} else {
 			callback(new Error("Not allowed by CORS"));
